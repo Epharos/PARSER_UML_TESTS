3b49a3ea354f0d97e4816a00eb70b95aab66b2b1
Aurélien "Epharos" REY <Neox30650@gmail.com>
2025-09-06 23:29:38 +0200

    Added a few classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+class A
+{
+	public:
+	A() {}
+	~A() {}
+	void foo() {}
+};
+
+class B : public A
+{
+public:
+	B() {}
+	~B() {}
+	void bar() {}
+};
+
+class C : public A
+{
+	public:
+	C() {}
+	~C() {}
+	void baz() {}
+};
+
+class D
+{
+	public:
+	D() {}
+	~D() {}
+	void qux() {}
+};
+
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
